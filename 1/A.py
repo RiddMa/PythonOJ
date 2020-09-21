c43bcec5980b318079fbb008b86981ae143f63b2
RiddMa <riddma@qq.com>
2020-09-21 20:34:41 +0800

    2020年9月21日作业1

--- 1/A.py
+++ 1/A.py
@@ -1,2 +1,10 @@
+# 题目描述
+# 输入两个整数a和b，计算a+b的和。
+# 输入
+# 两个用空格分隔的整数，分别代表a和b（0<a,b<10000）。
+# 输出
+# a+b的和。
+
 inputStr = input()
-print(inputStr)
+sourceNum = inputStr.split(" ")
+print(int(sourceNum[0])+int(sourceNum[1]))
